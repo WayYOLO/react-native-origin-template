755f3140977d74f1381693bb592cc5d54c399445
wb-wy449810 <wb-wy449810@alibaba-inc.com>
2019-01-17 14:45:56 +0800

    huskyrc test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n ' + ' Cmd+D or shake for dev menu',
+  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu'
 });
 
@@ -20,10 +20,10 @@ export default class App extends Component {
   };
 
   render() {
-    const { welcomeText } = this.state;
+    // const { welcomeText } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>{welcomeText}</Text>
+        <Text style={styles.welcome}>{this.state.welcomeText}</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
